1d8ba16d365b5d01e222123095292345b1b16e41
Gabriel <nobrecoder@gmail.com>
2025-12-19 06:28:20 -0300

    Updated in: 19/12/2025 06:28:20

--- dbs/flashcards_db.sql
+++ dbs/flashcards_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 17/12/2025 às 10:33
+-- Tempo de geração: 19/12/2025 às 10:27
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -41,7 +41,11 @@ CREATE TABLE `cards` (
 --
 
 INSERT INTO `cards` (`id`, `group_id`, `question`, `answer`, `answer_image`, `created_at`) VALUES
-(1, 1, 'Por que o Java pode ser comparado a uma matrioska?', NULL, 'photos/card_6942780967555.png', '2025-12-17 09:29:45');
+(1, 1, 'Por que o Java pode ser comparado a uma matrioska?', NULL, 'photos/card_6942780967555.png', '2025-12-17 09:29:45'),
+(4, 1, 'O que é o JDK?', 'O JDK (Java Development Kit) é o kit de ferramentas essencial para qualquer pessoa que deseja desenvolver aplicações em Java. Pense nele como uma \"caixa de ferramentas\" completa que contém tudo o que um programador precisa para escrever, compilar e testar código Java. Ele contém o JVM e JRE.', NULL, '2025-12-19 08:39:50'),
+(5, 1, 'O JDK não é um programa único, na realidade ele é dividido em 3 pilares: Ferramentas de Desenvolvimento, JRE e as Bibliotecas de Classe. Explique o que são \"As Ferramentas de Desenvolvimento\"', 'Ferramentas de Desenvolvimento: Programas de linha de comando que você usa para manipular seu código. Os principais são:\r\n* javac: O compilador que transforma seu código fonte (.java) em bytecode (.class).\r\n* jar: Ferramenta para empacotar seus arquivos em um único arquivo comprimido.\r\n* javadoc: Gerador automático de documentação a partir dos comentários do código.\r\n* jdb: Um depurador (debugger) para encontrar erros.\r\n* jvm: Máquina Virtual Java que lê o bytecode e traduz para a linguagem do seu computador.', NULL, '2025-12-19 08:46:20'),
+(6, 1, 'O JDK não é um programa único, na realidade ele é dividido em 3 pilares: Ferramentas de Desenvolvimento, JRE e as Bibliotecas de Classe. Explique o que é \"As Bibliotecas de Classe\"', 'Um vasto conjunto de códigos pré-escritos que lidam com tarefas comuns (como ler arquivos, conectar à internet ou criar listas), evitando que você tenha que \"reinventar a roda\".', NULL, '2025-12-19 09:18:01'),
+(7, 1, 'O que é o JRE?', 'JRE (Java Runtime Environment): O ambiente de execução. Se o JDK é para criar, o JRE é para rodar. O JDK já vem com um JRE embutido para que você possa testar o que acabou de programar.', NULL, '2025-12-19 09:18:56');
 
 -- --------------------------------------------------------
 
@@ -61,8 +65,7 @@ CREATE TABLE `groups` (
 --
 
 INSERT INTO `groups` (`id`, `name`, `description`, `created_at`) VALUES
-(1, 'Fundamentos do Java', NULL, '2025-12-17 09:28:43'),
-(2, 'Java Patterns', 'Sair do básico. Aqui você aprende as ferramentas modernas do Java (versões 8, 11, 17 e 21) e Padrões de Projeto (Design Patterns) que resolvem problemas arquiteturais comuns.', '2025-12-17 09:31:03');
+(1, 'Fundamentos do Java', NULL, '2025-12-17 09:28:43');
 
 --
 -- Índices para tabelas despejadas
@@ -89,7 +92,7 @@ ALTER TABLE `groups`
 -- AUTO_INCREMENT de tabela `cards`
 --
 ALTER TABLE `cards`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `groups`
